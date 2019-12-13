66c6c18ad22529743f43371fccb1c44db0454524
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2019-12-13 09:41:41 -0800

    Input form

--- smurfs/src/components/SmurfForm.js
+++ smurfs/src/components/SmurfForm.js
@@ -1,13 +1,39 @@
 import React from 'react';
+import useInput from '../hooks/useInput';
 
 function SmurfForm(props) {
+  const [name, setName, handleNameChange] = useInput('');
+  const [age, setAge, handleAgeChange] = useInput('');
+  const [height, setHeight, handleHeightChange] = useInput('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log('hello from handleSubmit!')
+    setName('');
+    setAge('');
+    setHeight('');
+  }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input
         placeholder='Smurf name'
         type='text'
-        value=''
+        value={name}
+        onChange={e => handleNameChange(e.target.value)}
       />
+      <input 
+        placeholder='age'
+        type='number'
+        value={age}
+        onChange={e => handleAgeChange(e.target.value)}
+      />
+      <input 
+        placeholder='height'
+        type='text'
+        value={height}
+        onChange={e => handleHeightChange(e.target.value)}
+      />
+      <button>Add your Smurf!</button>
     </form>
   );
 }
